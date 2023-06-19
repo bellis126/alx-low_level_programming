b06c1d0ab4c24a8c8ccee5a596b5a53790cb77c7
Prince Anane <ananeprince1@aol.com>
2023-06-19 09:13:02 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int b;
+
+for (b = 48; b < 58; b++)
+{
+putchar(b);
+}
+for (b = 97; b < 103; b++)
+{
+putchar(b);
+}
+putchar('\n');
+return (0);
+}
