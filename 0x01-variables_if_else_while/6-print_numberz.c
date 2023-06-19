851c9142dc449107826ee22558a830f6a7f1ce87
Prince Anane <ananeprince1@aol.com>
2023-06-19 08:39:31 -0700

    Numbers Again

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 48; b < 58; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
