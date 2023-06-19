c357c9a6da13482d9b598e828d487376ce708b5c
Prince Anane <ananeprince1@aol.com>
2023-06-19 07:09:26 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char d;
+
+char g;
+
+d = 'a';
+g = 'A';
+while
+(d <= 'z') {
+putchar(d);
+d++;
+}
+while
+(g <= 'z') {
+putchar (g);
+g++;
+}
+putchar('\n');
+return (0);
+}
