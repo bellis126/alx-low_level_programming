bd51f89c9a542dbab9113bc973997cc9f0dd7d58
Prince Anane <ananeprince1@aol.com>
2023-06-30 11:09:44 -0700

    multiples of 3 and 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,16 +8,17 @@
 int main(void)
 {
 
-int i, z = 0;
+int i = 0;
+int z = 0;
 
-for (i = 0; i < 1024; i++)
+while (i < 1024)
 {
 if ((i % 3 == 0) || (i % 5 == 0))
 {
-z += 1;
+z += i;
 }
+i++;
 }
-
 printf("%d\n", z);
 
 return (0);
