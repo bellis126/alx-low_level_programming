317cc9d52a0f986a9a9816553a3fb8dc049fd111
Prince Anane <ananeprince1@aol.com>
2023-06-30 11:33:15 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the first 52 fibonacci numbers
+ * Return: Nothing
+ */
+
+int main(void)
+
+{
+
+	int a = 0;
+	long b = 1, c = 2;
+
+	while (a < 50)
+	{
+		if (a == 0)
+			printf("%ld", b);
+		else if (a == 1)
+			printf(", %ld", c);
+		else
+		{
+			c += b;
+			b = c - j;
+			printf(", %ld", c);
+		}
+		++a;
+	}
+	printf("\n");
+	return (0);
+
+}
